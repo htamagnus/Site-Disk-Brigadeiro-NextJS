5d9170680548c049ab01242fe3dc36c8b7d31bc4
hta <agathamagnusg@hotmail.com>
2023-02-28 15:30:13 -0300

    animacao-fade-up-na-parte-sobre

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,8 @@ export default function About() {
   return (
     <>
       <div id="about" className={styles.about}>
-        <Image
+        <Image  data-aos="fade-up"
+            data-aos-duration="1000"
           priority
           width={560}
           height={540}
@@ -17,21 +18,24 @@ export default function About() {
           className={styles.about__imagem}
         ></Image>
         <nav className={styles.about__textos}>
-          <p>
+          <p  data-aos="fade-up"
+            data-aos-duration="1000">
             Nossa história começa em 2015, com a paixão pela confeitaria. Ao
             longo dos anos, desenvolvemos receitas únicas e deliciosas que
             encantam nossos clientes. Cada doce é feito com{" "}
             <strong>amor e dedicação</strong>, resultando em sabores e texturas
             incríveis.
           </p>
-          <p>
+          <p  data-aos="fade-up"
+            data-aos-duration="1000">
             Somos uma equipe apaixonada por criar doces artesanais e saborosos,
             feitos com ingredientes de alta qualidade e muito cuidado.
             Acreditamos que doces devem ser uma experiência{" "}
             <strong>única e memorável</strong>, capazes de proporcionar momentos
             de prazer e felicidade.
           </p>
-          <p>
+          <p  data-aos="fade-up"
+            data-aos-duration="1000">
             Cada doce que criamos é uma obra de arte, e estamos sempre buscando
             a perfeição em cada detalhe. Se você está procurando por doces
             gourmets de alta qualidade e sabor inigualável,{" "}
